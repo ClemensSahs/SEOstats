9606cb0d0f75577ee04063611e0ffebbb7f86057
Clemens Sahs <clemens.sahs@slide-worker.org>
2014-08-26 22:22:04 +0200

    add helper method to testCase

--- tests/SeoStatsV3Test/AbstractSeoStatsTestCase.php
+++ tests/SeoStatsV3Test/AbstractSeoStatsTestCase.php
@@ -10,4 +10,78 @@ abstract class AbstractSeoStatsTestCase extends \PHPUnit_Framework_TestCase
     {
         parent::setup();
     }
+
+
+    public function helperMakeAccessable ($object, $propertyOrMethod, $value = null)
+    {
+        $reflection = $this->helperGetReflectionObject($object);
+        $isMethod = $reflection->hasMethod($propertyOrMethod);
+
+        if ($isMethod) {
+            $reflectionSub = $reflection->getMethod($propertyOrMethod);
+        } else {
+            $reflectionSub = $reflection->getProperty($propertyOrMethod);
+        }
+
+        $reflectionSub->setAccessible(true);
+
+        if (!is_null($value)) {
+            if ($isMethod) {
+                return $reflectionSub->invokeArgs($object, $value);
+            } else {
+                $reflectionSub->setValue($object, $value);
+            }
+        }
+
+        return $reflectionSub;
+    }
+
+
+    public function helperSetAttributes ($object, $attributes)
+    {
+        foreach ($attributes as $attributeName => $attributeValue) {
+            $setterMethodName = $this->helperGetAttributesSetter($object, $attributeName);
+
+            $this->helperMakeAccessable($object,
+                                        $setterMethodName ?: $attributeName,
+                                        $setterMethodName ? array($attributeValue) : $attributeValue
+                                        );
+        }
+    }
+
+    public function helperGetAttributesSetter ($objectClass, $attribute)
+    {
+        $reflection = $this->helperGetReflectionObject($objectClass);
+
+        $methodeArray= array(
+            'set' . ucwords($attribute),
+            'set_' . $attribute,
+            $attribute
+        );
+
+        foreach ($methodeArray as $methodName) {
+            if (!$reflection->hasMethod($methodName)) {
+                continue;
+            }
+
+            return $methodName;
+        }
+        return false;
+    }
+
+    public function helperGetReflectionObject ($object)
+    {
+        if ( is_string($object) ) {
+            $objectClass = $object;
+            $object = null;
+        } else {
+            $objectClass = get_class($object);
+        }
+
+        if (!isset($this->reflection[$objectClass])) {
+            $this->reflection[$objectClass] = new ReflectionClass($objectClass);
+        }
+        return $this->reflection[$objectClass];
+    }
+
 }
