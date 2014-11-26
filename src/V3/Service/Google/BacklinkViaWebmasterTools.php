0ba27038202ff30a20c57ef47332e0e7b7d5b30d
Clemens Sahs <clemens.sahs@laborato.de>
2014-11-26 16:11:48 +0100

    add tests for BacklinkViaWebmasterTools

--- src/V3/Service/Google/BacklinkViaWebmasterTools.php
+++ src/V3/Service/Google/BacklinkViaWebmasterTools.php
@@ -2,10 +2,12 @@
 
 namespace SeoStats\V3\Service\Google;
 
+use SeoStats\V3\Model\PageInterface;
+
 class BacklinkViaWebmasterTools extends AbstractGoogleService
 {
-    public function call($url)
+    public function call(PageInterface $url)
     {
-        throw new \RuntimeException(sprintf('%s not implementet', __METHODE__));
+        throw new \RuntimeException(sprintf('%s not implementet', __METHOD__));
     }
 }
--- tests/SeoStatsV3Test/Service/Google/BacklinkViaWebmasterToolsTest.php
+++ tests/SeoStatsV3Test/Service/Google/BacklinkViaWebmasterToolsTest.php
@@ -18,9 +18,24 @@ class BacklinkViaWebmasterToolsTest extends AbstractGoogleApiTestCase
      * @group v3
      * @group service
      * @group service-google
+     * @expectedException RuntimeException
+     * @expectedExceptionMessage SeoStats\V3\Service\Google\BacklinkViaWebmasterTools::call not implementet
      */
     public function testBacklinkViaWebmasterTools ()
     {
-        $this->markTestIncomplete();
+        $url = 'www.github.com';
+        $page = new Page($url);
+
+        $this->SUT->call($page);
+    }
+
+    /**
+     * @group v3
+     * @group service
+     * @group service-google
+     */
+    public function testBacklinkViaWebmasterToolsLive ()
+    {
+        $this->markTestIncomplete("currently we have no code to tests...");
     }
 }
